22f813b282fb4b0c27b41c21b4b85a27615a3c04
Gaurav <gauravyadav2308@gmail.com>
2025-01-22 10:11:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Guess The Word</h1> 
+     <button id='startbutton'>Start Game</button>
+     <h2>Chances left:</h2>
+     <h2>Word Length:</h2>
+     <input></input>
+     <button id='guessbutton'> Guess</button><br/>
+     <button id='reloadbutton'>Reload Game</button>
+    </div>
+  );
+}
+
+export default App;
